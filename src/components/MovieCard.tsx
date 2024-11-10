6a0407bb857e52d8b07b3e825c839bdc3b44d5b4
Kwonyeojiny <kangagi0009@naver.com>
2024-11-11 04:16:12 +0900

    Design: MovieCard 포스터 비율 수정

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -5,19 +5,17 @@ const baseUrl = 'https://image.tmdb.org/t/p/w500';
 interface MovieCardProps {
   title: string;
   poster_path: string;
-  vote_average: number;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ title, poster_path, vote_average }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ title, poster_path }) => {
   return (
     <>
       <div className="w-64 p-4 border-4 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300 translate-all duration-300 hover:scale-105">
         <img
           src={`${baseUrl}${poster_path}`}
-          className="w-full aspect-[3/4] object-cover border-2 border-gray-500 border-l-gray-200 border-t-gray-200"
+          className="w-full aspect-[2/3] object-cover border-2 border-gray-500 border-l-gray-200 border-t-gray-200"
         />
-        <h3 className="text-lg font-bold ml-2 my-4">{title}</h3>
-        <div className="text-sm text-right text-gray-500">평점: {vote_average}</div>
+        <h3 className="font-bold ml-2 mt-2">{title}</h3>
       </div>
     </>
   );
