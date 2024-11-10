4ffac5521b7ba68ed4911a1d090358113a1645ab
Kwonyeojiny <kangagi0009@naver.com>
2024-11-10 19:22:22 +0900

    Feat: movieDetail Tmdb api 연결

--- src/pages/MoviDetail.tsx
+++ src/pages/MoviDetail.tsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import movieDetail from '../data/movieDetailData.json';
+import { fetchMovieDetail } from '../api/tmdbApi';
+import { useParams } from 'react-router-dom';
 
 const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
 const MoveiDetail: React.FC = () => {
-  const [movieData, setMovieData] = useState<any>({});
+  const [movieData, setMovieData] = useState<any>([]);
+  const { id } = useParams<{ id: string }>();
+  const [loading, setLoading] = useState<boolean>(true);
 
   // 구조분해할당
   const { poster_path, title, vote_average, genres, overview } = movieData || {};
 
   useEffect(() => {
-    setMovieData(movieDetail);
+    const getMovieData = async () => {
+      if (id) {
+        setLoading(true);
+        const data = await fetchMovieDetail(id);
+        setMovieData(data);
+        setLoading(false);
+      }
+    };
+    getMovieData();
   }, []);
+
+  if (loading) return <div>Loading...</div>;
 
   if (!movieData) {
     return <div>영화 데이터가 없습니다</div>;
@@ -19,14 +32,14 @@ const MoveiDetail: React.FC = () => {
 
   return (
     <>
-      <div className="m-8 grid gap-4 p-4 border-4 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300 md:grid-cols-2">
+      <div className="mt-16 m-8 grid gap-4 p-4 border-4 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300 md:grid-cols-2">
         <img
           src={`${baseUrl}${poster_path}`}
           className="w-full border-4 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300"
         />
-        <div>
-          <div className="flex items-center justify-between">
-            <h2 className="text-2xl">{title}</h2>
+        <div className="flex flex-col gap-4">
+          <div className="mt-4 flex items-center justify-between">
+            <h2 className="text-2xl font-bold">{title}</h2>
             <p>평점: {vote_average}</p>
           </div>
           <ul className="flex gap-4 my-4 list-none justify-center">
@@ -34,7 +47,7 @@ const MoveiDetail: React.FC = () => {
               genres.map((genre: any, index: number) => (
                 <li
                   key={index}
-                  className="p-0.5 border-2 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300"
+                  className="py-0.5 px-2 border-2 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300"
                 >
                   {genre.name}
                 </li>
