0ac0e71124b5d5f58867c6662ff132bdba9e23d0
Kwonyeojiny <kangagi0009@naver.com>
2024-11-10 18:53:37 +0900

    Feat: popular 영화 리스트 Tmdb api 연결

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MovieCard from '../components/MovieCard';
-import movieDatas from '../data/movieListData.json';
 import { Link } from 'react-router-dom';
+import { fetchPopularMovies } from '../api/tmdbApi';
 
 const MovieList: React.FC = () => {
+  const [movies, setMovies] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const data = await fetchPopularMovies();
+      setMovies(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div className="flex flex-wrap justify-center items-center gap-8 mt-16">
-        {movieDatas.results.map(movie => (
-          <Link to={`/details/${movie.id}`} key={movie.id}>
-            <MovieCard
-              title={movie.title}
-              poster_path={movie.poster_path}
-              vote_average={movie.vote_average}
-            />
-          </Link>
-        ))}
+        {movies &&
+          movies.map(movie => (
+            <Link to={`/details/${movie.id}`} key={movie.id}>
+              <MovieCard
+                title={movie.title}
+                poster_path={movie.poster_path}
+                vote_average={movie.vote_average}
+              />
+            </Link>
+          ))}
       </div>
     </>
   );
