b44106642c46f19687794800dafdf20e644d30e6
Kwonyeojiny <kangagi0009@naver.com>
2024-11-16 20:13:18 +0900

    Feat: 평점 swiper 추가

--- src/components/MovieTopRated.tsx
+++ src/components/MovieTopRated.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import { fetchTopRatedMovies } from '../api/tmdbApi';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, A11y, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
@@ -28,19 +34,46 @@ const MovieTopRated = () => {
       <div className="underline my-4 text-2xl sm:text-3xl text-white text-shadow drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
         Rating Top 20
       </div>
-      {/* 최고 평점 스와이퍼 Top 10? 20? */}
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {movies.map(movie => (
-          <Link to={`/details/${movie.id}`} key={movie.id} className="flex justify-center">
-            <div className="w-full max-w-[280px] p-2 border-4 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300 transition-all duration-300 hover:scale-105">
-              <img
-                src={`${baseUrl}${movie.poster_path}`}
-                alt={movie.title}
-                className="w-full aspect-[2/3] object-cover border-4 border-gray-200 border-l-gray-500 border-t-gray-500"
-              />
-            </div>
-          </Link>
-        ))}
+      <div className=" bg-gray-300 border-4 border-gray-500 border-l-gray-200 border-t-gray-200">
+        <Swiper
+          modules={[Pagination, A11y, Autoplay]}
+          slidesPerView={3}
+          loop
+          navigation
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          breakpoints={{
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+            1280: {
+              slidesPerView: 5,
+            },
+          }}
+        >
+          {movies.map((movie, index) => (
+            <SwiperSlide key={movie.id}>
+              <Link to={`/details/${movie.id}`} className="flex justify-center">
+                <div className="w-full max-w-[280px] m-4 p-2 border-4 border-gray-500 border-l-gray-200 border-t-gray-200 bg-gray-300 transition-all duration-300 hover:scale-105">
+                  <h1>
+                    Top <span className="text-xl sm:text-2xl ">{index + 1}</span>
+                  </h1>
+                  <img
+                    src={`${baseUrl}${movie.poster_path}`}
+                    alt={movie.title}
+                    className="w-full aspect-[2/3] object-cover border-4 border-gray-200 border-l-gray-500 border-t-gray-500"
+                  />
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   );
